8a9a681b6044e3f95937abbc8d35dea76d732f85
nikita <you@example.com>
2020-06-15 00:11:49 +0300

    third task

--- src/index.js
+++ src/index.js
@@ -4,7 +4,9 @@ import './style.css'
 import svg from './assets/svgs/sasha.svg'
 const myImage = new Image();
 myImage.src = image;
-const arr = map(['','3', '%'], (el)=>`<div class="text">${el}</div>`);
-document.querySelector('#root').innerHTML = arr.join('');
-document.querySelector('#root').prepend(myImage);
+const arr = map(['3', '%'], (el)=>`<div class="text">${el}</div>`);
+const el = document.createElement('div');
+el.innerHTML = arr.join('');
+el.prepend(myImage);
+document.body.append(el);
 document.querySelector('.text').style.background =  `url(${svg}) no-repeat`
--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,7 @@
 const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const {CleanWebpackPlugin} = require('clean-webpack-plugin');
+
 
 module.exports = {
   entry: './src/index.js',
@@ -23,4 +26,7 @@ module.exports = {
       },
     ],
   },
+  plugins: [new CleanWebpackPlugin(),
+    new HtmlWebpackPlugin(),
+  ]
 };
