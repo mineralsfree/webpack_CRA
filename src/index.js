4e7752c3a7117c115e9b77fedaefa05b78cb9c8f
nikita <you@example.com>
2020-06-14 23:48:12 +0300

    task 2

--- src/index.js
+++ src/index.js
@@ -1,7 +1,10 @@
 import * as map from 'lodash/map';
-
-const arr = map(['3', '%'], (el)=>`<div class="text">${el}<div>`);
-
-const COLOR = "red"
-document.body.style.background = COLOR;
+import image from './assets/imgs/main.png'
+import './style.css'
+import svg from './assets/svgs/sasha.svg'
+const myImage = new Image();
+myImage.src = image;
+const arr = map(['','3', '%'], (el)=>`<div class="text">${el}</div>`);
 document.querySelector('#root').innerHTML = arr.join('');
+document.querySelector('#root').prepend(myImage);
+document.querySelector('.text').style.background =  `url(${svg}) no-repeat`
